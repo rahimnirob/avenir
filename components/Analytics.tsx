76cd2a03274cde49764c581615f3632a7d2ad477
rahimnirob <rahimnirob420@gmail.com>
2025-10-22 23:13:52 +0600

    Create Analytics.tsx

---
+++ components/Analytics.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export default function Analytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (typeof window.gtag !== "undefined") {
+      const url = pathname + (searchParams ? `?${searchParams.toString()}` : "");
+      window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, { page_path: url });
+    }
+  }, [pathname, searchParams]);
+
+  return null;
+}
